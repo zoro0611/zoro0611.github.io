361a5e98a7716f00cd31057bb6f26e7aef8c3ec2
Leo_Hsu <zoro061181249@gmail.com>
2023-02-19 22:01:04 +0800

    update script_scoped and slot

---
+++ VuePractice/Courses/第9節_Component_04/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- <the-header></the-header> -->
+    <TheHeader />
+    <BadgeList />
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import BadgeList from './components/BadgeList.vue'
+import UserInfo from './components/UserInfo.vue'
+export default {
+  components:{
+    // 'the-header': TheHeader
+    TheHeader,
+    BadgeList,
+    // 'user-info': UserInfo,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
