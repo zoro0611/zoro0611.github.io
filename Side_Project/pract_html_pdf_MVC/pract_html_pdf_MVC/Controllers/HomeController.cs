d6967e5d311e0537ed00bd7ca31eba70dd1530e2
Leo_Hsu <zoro061181249@gmail.com>
2023-06-05 23:26:25 +0800

    update side project1_html_to_pdf

---
+++ Side_Project/pract_html_pdf_MVC/pract_html_pdf_MVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pract_html_pdf_MVC.Models;
+using System.Diagnostics;
+
+using SelectPdf;
+
+namespace pract_html_pdf_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["test"] = "假裝打資料進入頁面";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public FileResult ExportPdf(string htmlData)
+        {
+            HtmlToPdf objHtml = new HtmlToPdf();
+            PdfDocument objDoc = objHtml.ConvertHtmlString(htmlData);
+            byte[] pdf = objDoc.Save();
+            objDoc.Close();
+            return File(pdf, "application/pdf", "Test.pdf");
+
+        }
+
+    }
+}
